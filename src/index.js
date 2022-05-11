0b0a8bcc487b3eebdfdaa0a84ccf2f4ff9b823d5
Jonathan Cole <jon@jcole.io>
2022-05-11 11:53:55 -0400

    comments

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const ini = require('ini');
 const fs = require('fs');
 const exec = require('child_process').exec;
 
+// Hosts: e.g. "alias1,ip1,port1_alias2,ip2,port2"
 const {hosts: hostString, timeoutSeconds, runIntervalSeconds, assertEveryHours} = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
 
 const hostPairs = hostString.split("_");
@@ -14,9 +15,9 @@ const hosts = hostPairs.map(hostPair => {
 console.log("configured hosts: ", hosts)
 
 var lastOutput = null;
-
 var assertAfter = null;
 
+// use netcat to see if the ip is listening at that port.  Return a promise that resolves true if yes, resolves false if no, rejects if any error
 function getOutput(ip, port) {
 	return new Promise((resolve, reject) => {
 		exec(`nc -zw ${timeoutSeconds} ${ip} ${port}; echo $?`, function(error, stdout, stderr) {
@@ -28,6 +29,11 @@ function getOutput(ip, port) {
 	});
 }
 
+/**
+ * 
+ * @param {*} output Array of booleans that lines up with the array of hosts
+ * @returns A single string to output to the log (and test for changes)
+ */
 function stringifyOutput(output) {
 	return output.map((e, i) => {
 		const host = hosts[i];
@@ -35,12 +41,13 @@ function stringifyOutput(output) {
 	}).join("; ")
 }
 
+// Kick the next assert time forward
 function bumpAssertTime() {
 	assertAfter = (new Date()).getTime() + (1000 * 60 * 60 * assertEveryHours);
 }
 
-
 function loop() {
+	// fire off netcat calls to all defined ips/ports
 	Promise.all(hosts.map(h => getOutput(h.ip, h.port))).then(
 		(results) => {
 			const date = new Date();
